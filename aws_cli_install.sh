6a466186b297882c0f482c741e67d6f8909ce8cb
Aaron Dean <adean20csc@gmail.com>
2016-05-17 10:43:15 -0400

    Create aws_cli_install.sh

---
+++ aws_cli_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ~
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+
+unzip awscli-bundle.zip
+
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
